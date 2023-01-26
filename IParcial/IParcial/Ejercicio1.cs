50445f8623eac8422f08e6d1d2552e0cd6db8070
Luis20041009894 <hakudoushy19@hotmail.com>
2023-01-25 23:57:55 -0600

    Ejercicio2
    Ejercicio de funciones y procedimientos

--- IParcial/IParcial/Ejercicio1.cs
+++ IParcial/IParcial/Ejercicio1.cs
@@ -21,6 +21,7 @@ namespace IParcial
         {
 
         }
+        decimal Sumatotal = 0;
 
         private void SumarButton_Click(object sender, EventArgs e)
         {
@@ -28,17 +29,47 @@ namespace IParcial
             decimal numero1 = Convert.ToDecimal(Numero1TextBox.Text);
             decimal numero2 = Convert.ToDecimal(Numero2TextBox.Text);
 
-            decimal resultado = numero1 + numero2;
+            //decimal resultado = numero1 + numero2;
 
             //MessageBox.Show("LA SUMA ES: " + resultado);
 
-            ResultadoLabel.Text = resultado.ToString();
+            //ResultadoLabel.Text = resultado.ToString();
 
+            //Sumar(numero1, numero2);
 
-
-
-
+            Sumatotal = Sumatoria(numero1, numero2);
+            MessageBox.Show("La suma es: " + Sumatotal, "Resultado", MessageBoxButtons.OK,MessageBoxIcon.Information);
 
         }
+        //Procedimiento
+        private void Sumar(decimal num1,decimal num2)
+
+        {
+            Sumatotal = num1 + num2;
+
+        }
+
+        //Función    
+
+        private decimal Sumatoria(decimal num1, decimal num2)
+
+        {
+            //decimal n1, n2, suma;
+            //n1 = num1;
+            //n2 = num2;
+            
+            //suma = n1+n2;
+            //return suma;
+
+
+            return num1 + num2;       
+                
+        }
+
+
+            
     }
+
+
+
 }
---
+++ IParcial/IParcial/Ejercicio2.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace IParcial
+{
+    partial class Ejercicio2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EjecutarButton = new System.Windows.Forms.Button();
+            this.OperacionesComboBox = new System.Windows.Forms.ComboBox();
+            this.Numero1TextBox = new System.Windows.Forms.TextBox();
+            this.Numero2TextBox = new System.Windows.Forms.TextBox();
+            this.ResultadoLabel = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero 2:";
+            // 
+            // EjecutarButton
+            // 
+            this.EjecutarButton.Location = new System.Drawing.Point(198, 232);
+            this.EjecutarButton.Name = "EjecutarButton";
+            this.EjecutarButton.Size = new System.Drawing.Size(236, 47);
+            this.EjecutarButton.TabIndex = 2;
+            this.EjecutarButton.Text = "Ejecutar";
+            this.EjecutarButton.UseVisualStyleBackColor = true;
+            this.EjecutarButton.Click += new System.EventHandler(this.EjecutarButton_Click);
+            // 
+            // OperacionesComboBox
+            // 
+            this.OperacionesComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.OperacionesComboBox.FormattingEnabled = true;
+            this.OperacionesComboBox.Items.AddRange(new object[] {
+            "Suma",
+            "Resta",
+            "Multiplicación",
+            "División"});
+            this.OperacionesComboBox.Location = new System.Drawing.Point(198, 174);
+            this.OperacionesComboBox.Name = "OperacionesComboBox";
+            this.OperacionesComboBox.Size = new System.Drawing.Size(236, 37);
+            this.OperacionesComboBox.TabIndex = 3;
+            // 
+            // Numero1TextBox
+            // 
+            this.Numero1TextBox.Location = new System.Drawing.Point(198, 70);
+            this.Numero1TextBox.Name = "Numero1TextBox";
+            this.Numero1TextBox.Size = new System.Drawing.Size(100, 34);
+            this.Numero1TextBox.TabIndex = 4;
+            // 
+            // Numero2TextBox
+            // 
+            this.Numero2TextBox.Location = new System.Drawing.Point(198, 121);
+            this.Numero2TextBox.Name = "Numero2TextBox";
+            this.Numero2TextBox.Size = new System.Drawing.Size(100, 34);
+            this.Numero2TextBox.TabIndex = 5;
+            // 
+            // ResultadoLabel
+            // 
+            this.ResultadoLabel.AutoSize = true;
+            this.ResultadoLabel.Location = new System.Drawing.Point(491, 249);
+            this.ResultadoLabel.Name = "ResultadoLabel";
+            this.ResultadoLabel.Size = new System.Drawing.Size(122, 29);
+            this.ResultadoLabel.TabIndex = 6;
+            this.ResultadoLabel.Text = "Resultado";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Ejercicio2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 468);
+            this.Controls.Add(this.ResultadoLabel);
+            this.Controls.Add(this.Numero2TextBox);
+            this.Controls.Add(this.Numero1TextBox);
+            this.Controls.Add(this.OperacionesComboBox);
+            this.Controls.Add(this.EjecutarButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Ejercicio2";
+            this.Text = "Ejercicio2";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button EjecutarButton;
+        private System.Windows.Forms.ComboBox OperacionesComboBox;
+        private System.Windows.Forms.TextBox Numero1TextBox;
+        private System.Windows.Forms.TextBox Numero2TextBox;
+        private System.Windows.Forms.Label ResultadoLabel;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
