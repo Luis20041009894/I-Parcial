4a11811f55889c39f7a56db291e5f0a206b7fc14
Luis20041009894 <hakudoushy19@hotmail.com>
2023-02-24 10:12:28 -0600

    Proyecto
    commit

---
+++ Proyecto facturación/Facturacion/Entidades/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+
+namespace Entidades
+{
+    public class Usuario
+    {
+        public string CodigoUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Contraseña { get; set; }
+        public string Correo { get; set; }
+        public string Rol { get; set; }
+        public DataSetDateTime FechaCreacion { get; set; }
+        public bool EstadoActivo { get; set; }
+
+        public Usuario()
+        {
+        }
+
+        public Usuario(string codigoUsuario, string nombre, string contraseña, string correo, string rol, DataSetDateTime fechaCreacion, bool estadoActivo)
+        {
+            CodigoUsuario = codigoUsuario;
+            Nombre = nombre;
+            Contraseña = contraseña;
+            Correo = correo;
+            Rol = rol;
+            FechaCreacion = fechaCreacion;
+            EstadoActivo = estadoActivo;
+        }
+    }
+}
---
+++ Proyecto facturación/Facturacion/Vista/Login.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelarButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void AceptarButton_Click(object sender, EventArgs e)
+        {
+            if (UsuarioTextBox.Text == string.Empty)
+            {
+                errorProvider1.SetError(UsuarioTextBox, "Ingrese su nombre de usuario");
+                UsuarioTextBox.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+
+            if (ContraseñaTextBox.Text == string.Empty)
+            {
+                errorProvider1.SetError(ContraseñaTextBox, "Ingrese su contraseña");
+                ContraseñaTextBox.Clear();
+                ContraseñaTextBox.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+
+            //Validar en la base de datos
+
+            // Mostrar el Menu
+
+            Menu menuFormulario = new Menu();
+            Hide();
+            menuFormulario.Show();
+
+        }
+
+        private void MostrarButton_Click(object sender, EventArgs e)
+        {
+            if (ContraseñaTextBox.PasswordChar == '*')
+            {
+                ContraseñaTextBox.PasswordChar = '\0';
+
+            }
+            else
+            {
+                ContraseñaTextBox.PasswordChar += '*';
+            }
+        }
+    }
+}
